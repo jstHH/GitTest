5158f9da0051aedfcd6eb734b8b16789beb5ea1f
Joachim <joachim.stolle@hotmail.de>
2022-03-22 16:13:46 +0100

    Test

---
+++ src/jst/gittest/GitTest.java
@@ -0,0 +1,9 @@
+package jst.gittest;
+
+import javax.swing.*;
+
+public class GitTest {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Git Test");
+    }
+}
